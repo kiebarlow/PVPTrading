b510c55a59e248df002f0613af9c7068f99473b1
SebastianStankiewicz <sebstankiewicz23@gmail.com>
2025-02-08 14:45:59 +0000

    icons added.

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Outlet, Link, useOutletContext } from "react-router-dom";
+import { FaPiggyBank, FaTrophy, FaHouse } from "react-icons/fa6";
 
 export default function Header() {
   return (
@@ -10,9 +11,23 @@ export default function Header() {
 
       {/* Navigation */}
       <nav className="hidden md:flex space-x-6 text-gray-300 text-sm font-medium">
-        <Link to={`browseGames`}>Browse Games</Link>
-        <Link to={`leaderBoard`}>Leaderboard</Link>
-        <Link to={`bank`}>Bank</Link>
+  
+        <Link to="browseGames" className="flex items-center space-x-2">
+          <div className="text-lg font-semibold">Browse Games</div>
+          <FaHouse className="text-xl" />
+        </Link>
+
+        
+
+        <Link to="leaderBoard" className="flex items-center space-x-2">
+          <div className="text-lg font-semibold">Leaderboard</div>
+          <FaTrophy className="text-xl" />
+        </Link>
+
+        <Link to="bank" className="flex items-center space-x-2">
+          <div className="text-lg font-semibold">Bank</div>
+          <FaPiggyBank className="text-xl" />
+        </Link>
       </nav>
 
       {/* Connect Wallet Placeholder */}
--- frontEnd/src/routes/GameBrowser.jsx
+++ frontEnd/src/routes/GameBrowser.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import GameCard from "../components/gameBrowseromponents/gameCard";
 import PreviousGameCard from "../components/gameBrowseromponents/PreviousGameCard";
+import { FaRegGem } from "react-icons/fa6";
+
 function GameBrowser() {
   return (
     <div className="min-h-screen bg-[#0D0D0D] text-white p-8">
@@ -12,7 +14,7 @@ function GameBrowser() {
         <h1 className="text-6xl font-semibold mb-4 CPMono">Live Games</h1>
         <div className="flex flex-wrap gap-5">
           <div className="bg-[#0D0D0D] border border-[#1E1E1E] rounded-xl shadow-md p-4 w-64 text-white text-sm flex flex-col gap-2">
-            <div className="text-lg font-semibold">Gem Entry Price: </div>
+            <div className="text-lg font-semibold">Entry Price:</div>
             <button className="mt-2 bg-blue-600 hover:bg-green-700 text-white font-medium py-1.5 rounded-lg transition">
               Create Game
             </button>
