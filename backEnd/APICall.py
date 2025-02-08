d15835d2c91e89ab2c4fb05eb3880f1cf4b32d83
Barlow Kieran (2023) ZMAC466 <kieran.barlow.2023@live.rhul.ac.uk>
2025-02-08 19:29:41 +0000

    API calls for gems depo and withdraw

---
+++ backEnd/APICall.py
@@ -0,0 +1,32 @@
+from solanaStuff import SolanaStuff
+from Database import *
+
+def DepositGems(usrName):
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT WalletID FROM User WHERE UserName = ?", (usrName))
+    wallID = cursor.fetchall()
+    cursor.execute("SELECT * FROM Wallet WHERE WalletID = ?",(wallID))
+    wallInfo = cursor.fetchall()
+    depo = SolanaStuff.checkForDeposit(wallInfo[1], wallInfo[2])
+    addGems(usrName,depo)
+    conn.close()
+    numGems = checkNumOfGems(usrName)
+    return numGems
+    """Now add a retrieve number of gems.
+    """
+    
+    
+
+def WithdrawSol(usrName, solAdress, numOfGems):
+    numGems = checkNumOfGems(usrName)
+    if numGems<numOfGems:
+        return False
+    elif SolanaStuff.withdrawBalance(numOfGems,solAdress):
+        withdrawGems(usrName,numOfGems)
+        return True
+    else:
+        return False
+    """Return true or false
+    """
+    
