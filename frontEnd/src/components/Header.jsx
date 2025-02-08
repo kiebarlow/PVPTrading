4f570f2fcc04788324f5e308459cd2d9ae9636f6
JacobCoding00 <jg4g23@soton.ac.uk>
2025-02-08 15:08:09 +0000

    fixing for merge

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,21 +1,40 @@
+import { Outlet, Link, useOutletContext } from "react-router-dom";
+import { FaPiggyBank, FaTrophy, FaHouse } from "react-icons/fa6";
+
 export default function Header() {
-    return (
-      <header className="bg-gradient-to-b from-black to-[#0a1f0b] p-4 shadow-md flex justify-between items-center">
-        {/* Logo */}
-        <div className="text-white text-xl font-bold tracking-wide">
-          PVPTrading.<span className="text-blue-500">Club</span>
-        </div>
+  return (
+    <header className="bg-gradient-to-b from-black to-[#0a1f0b] p-4 shadow-md flex justify-between items-center">
+      {/* Logo */}
+      <div className="text-white text-xl font-bold tracking-wide">
+        PVPTrading.<span className="text-blue-500">Club</span>
+      </div>
+
+      {/* Navigation */}
+      <nav className="hidden md:flex space-x-6 text-gray-300 text-sm font-medium">
+  
+        <Link to="browseGames" className="flex items-center space-x-2">
+          <div className="text-lg font-semibold">Browse Games</div>
+          <FaHouse className="text-xl" />
+        </Link>
+
         
-        {/* Navigation */}
-        <nav className="hidden md:flex space-x-6 text-gray-300 text-sm font-medium">
-          <span className="hover:text-white transition">Browse Games</span>
-          <span className="hover:text-white transition">Leaderboard</span>
-          <span className="hover:text-white transition">Bank</span>
-        </nav>
-        
-        {/* Connect Wallet Placeholder */}
-        <span className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md">Connect Wallet</span>
-      </header>
-    );
-  }
+
+        <Link to="leaderBoard" className="flex items-center space-x-2">
+          <div className="text-lg font-semibold">Leaderboard</div>
+          <FaTrophy className="text-xl" />
+        </Link>
+
+        <Link to="bank" className="flex items-center space-x-2">
+          <div className="text-lg font-semibold">Bank</div>
+          <FaPiggyBank className="text-xl" />
+        </Link>
+      </nav>
+
+      {/* Connect Wallet Placeholder */}
+      <span className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md">
+        Connect Wallet
+      </span>
+    </header>
+  );
+}
   
